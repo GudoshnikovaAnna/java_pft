b7467c7adc053a2e703bd6454711f430e0be5704
GudoshnikovaAnna <gudoshnikova.anna@gmail.com>
2021-07-12 11:49:58 +0300

    Реализация расстояния между двумя точками в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -10,26 +10,16 @@ public class MyFirstProgram {
 		Rectangle r = new Rectangle(5,6);
 		System.out.println(r.area());
 
-		Point p1 = new Point();
-		Point p2 = new Point();
-		p1.x = 2;
-		p1.y = 2;
-		p2.x = 4;
-		p2.y = 1;
-		System.out.println("Расстояние между точкой (2,2) и (4,1) = " + distance(p1, p2));
+		Point p1 = new Point(2,2);
+		Point p2 = new Point(4,1);
+		System.out.println("Расстояние между точкой (2,2) и (4,1) = " + p1.distance(p1, p2));
 
-		Point p3 = new Point();
-		Point p4 = new Point();
-		p3.x = 1;
-		p3.y = -2;
-		p4.x = 2;
-		p4.y = -2;
-		System.out.println("Расстояние между точкой (1,2) и (2,2) = " + distance(p3, p4));
+		Point p3 = new Point(1,-2);
+		Point p4 = new Point(2, -2);
+		System.out.println("Расстояние между точкой (1,2) и (2,2) = " + p3.distance(p3, p4));
 	}
 
-	public static double distance(Point p1, Point p2) {
-		return Math.sqrt(Math.pow((p1.x -p2.x),2) + Math.pow((p1.y-p2.y),2));
-	}
+
 
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,4 +4,13 @@ public class Point {
 
     public int x;
     public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p1.x -p2.x),2) + Math.pow((p1.y-p2.y),2));
+    }
 }
