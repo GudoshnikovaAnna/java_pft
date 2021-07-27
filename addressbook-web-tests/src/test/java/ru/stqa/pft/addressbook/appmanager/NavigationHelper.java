e84b6e77b8ea12b100f7b7a006b1ba1d6d441f8e
GudoshnikovaAnna <gudoshnikova.anna@gmail.com>
2021-07-27 13:45:50 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,12 +3,17 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
     public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && !wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && !isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
@@ -17,6 +22,11 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoAllContactsPage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
+
+
 }
