5bbf0599c9d06c54821b00778275c48f66e57f44
GudoshnikovaAnna <gudoshnikova.anna@gmail.com>
2021-08-23 12:17:25 +0300

    Добавлено аннотирование контактов для работы с БД

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,38 +1,81 @@
 package ru.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
+
+    @Id
+    @Column(name = "id")
     private int id = Integer.MAX_VALUE;
+
     @Expose
+    @Column(name = "firstname")
     private String contactFirstName;
+
     @Expose
+    @Column(name = "lastName")
     private String contactLastName;
+
     @Expose
+    @Column(name = "company")
     private String contactCompany;
+
     @Expose
+    @Column(name = "address")
+    @Type(type = "text")
     private String contactAddress;
+
     @Expose
+    @Column(name = "email")
+    @Type(type = "text")
     private String contactEmail;
+
+    @Column(name = "email2")
+    @Type(type = "text")
     private String contactEmail2;
+
+    @Column(name = "email3")
+    @Type(type = "text")
     private String contactEmail3;
+
     @Expose
+    @Transient
     private String group;
+
+    @Column(name = "home")
+    @Type(type = "text")
     private String homePhone;
+
+    @Column(name = "mobile")
+    @Type(type = "text")
     private String mobilePhone;
+
+    @Column(name = "work")
+    @Type(type = "text")
     private String workPhone;
+
+    @Transient
     private String allPhones;
+
+    @Transient
     private String allEmails;
-    private File photo;
+
+    @Column(name = "photo")
+    @Type(type = "text")
+    private String photo;
 
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
