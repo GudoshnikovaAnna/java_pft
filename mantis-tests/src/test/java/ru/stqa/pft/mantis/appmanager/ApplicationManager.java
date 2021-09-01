49195c44cd1530e4f741cd4844aad40f579fc0ce
GudoshnikovaAnna <gudoshnikova@vtb.ru>
2021-09-01 13:19:22 +0300

    Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -20,6 +20,7 @@ public class ApplicationManager {
     public final Properties properties;
     private RegistrationHelper registrationHelper;
     private FtpHelper ftp;
+    private MailHelper mailHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -68,5 +69,10 @@ public class ApplicationManager {
             wd.get(properties.getProperty("web.baseUrl"));
         }
         return wd;
+    }
+
+    public MailHelper mail() {
+        if (mailHelper == null) mailHelper = new MailHelper(this);
+        return mailHelper;
     }
 }
