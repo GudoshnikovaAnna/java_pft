f9286af3425e37f76ae9189360ac3ca1a35f1db7
GudoshnikovaAnna <gudoshnikova@vtb.ru>
2021-09-09 17:31:16 +0300

    Добавлена возможность пропускать тест, если статус дефекта не "Закрыт" - bugify

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -7,6 +7,8 @@ import com.google.gson.reflect.TypeToken;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
+import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -46,6 +48,67 @@ public class RestTests {
                 .returnContent().asString();
         JsonElement parsed = JsonParser.parseString(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+
+    private Issue getIssue(int issueId) throws IOException {
+        String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues/"+ issueId + ".json")).returnContent().asString();
+        JsonElement parsed = JsonParser.parseString(json);
+        JsonElement issue = parsed.getAsJsonObject().get("issues");
+        Set<Issue> oneIssue = new Gson().fromJson(issue, new TypeToken<Set<Issue>>() {}.getType());
+        Issue gettedissue = oneIssue.iterator().next();
+        return gettedissue;
+    }
+
+    private void closeIssue(int issueId) throws IOException {
+         getExecutor()
+                .execute(Request.Post("https://bugify.stqa.ru/api/issues/"+ issueId + ".json")
+                        .bodyForm(new BasicNameValuePair("method", "update"),
+                new BasicNameValuePair("issue[state]", "3")))
+                .returnContent().asString();
+        assertEquals(getIssue(issueId).getState_name(), "Closed");
+    }
+
+    private void updateDescriptionIssue(int issueId) throws IOException {
+            getExecutor()
+                .execute(Request.Post("https://bugify.stqa.ru/api/issues/"+ issueId + ".json")
+                        .bodyForm(new BasicNameValuePair("method", "update"),
+                                new BasicNameValuePair("issue[description]", "Update Description")))
+                .returnContent().asString();
+        assertEquals(getIssue(issueId).getDescription(), "Update Description");
+    }
+
+
+    public boolean isIssueOpen(int issueId) throws IOException {
+        Issue issue = getIssue(issueId);
+        if (issue.getState_name().equals("Closed")) return false;
+        else return true;
+    }
+
+    public void skipIfNotFixed(int issueId) throws IOException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+
+    @Test
+    public void runningAutotestIfIssueClosed() throws IOException {
+        Set<Issue> allIssues = getIssues();
+        Issue issue = allIssues.iterator().next();
+        System.out.println("ID= " + issue.getId());
+        if (isIssueOpen(issue.getId())) closeIssue(issue.getId());
+        skipIfNotFixed(issue.getId());
+        System.out.println("выполняется код автотеста");
+        updateDescriptionIssue(issue.getId());
+    }
+
+    @Test
+    public void runningAutotestIfIssueOpen() throws IOException {
+        Set<Issue> allIssues = getIssues();
+        Issue issue = allIssues.iterator().next();
+        System.out.println("ID= " + issue.getId());
+        skipIfNotFixed(issue.getId());
+        System.out.println("выполняется код автотеста");
     }
 
 }
